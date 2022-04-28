a2d69345dc79244164422d700bc47be30acf57a5
DangReus <khanhdangkp@gmail.com>
2022-04-28 20:46:42 +0700

    Change index module Print

--- modules/In/Views/index.blade.php
+++ modules/In/Views/index.blade.php
@@ -16,8 +16,46 @@
             </div>
         </div>
         <div class="card">
+            <div class="card-header">
+                <button id="btn-maudon" class="btn btn-primary font-18" style="width: 150px">Mẫu đơn</button>
+                <button id="btn-mauphieu" class="btn btn-primary font-18" style="width: 150px">Mẫu phiếu</button>
+            </div>
             <div class="card-body">
-                <div class="row font-20">
+                <div id="maudon" class="row font-20">
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Tổng hợp đề xuất mức chất lượng đảng viên của các chủ thể
+                        </a>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Quyết định - Công nhận xếp loại chất lượng đảng viên
+                        </a>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Quyết định - Về việc khen thưởng đảng viên
+                        </a>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Quyết định - Xếp loại chất lượng các chi bộ trực thuộc
+
+                        </a>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Quyết định - Tặng giấy khen cho chi bộ trực thuộc Đảng ủy cơ sở
+                        </a>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <a href="#" class="btn btn-info">
+                            Danh sách đảng viên chưa kiểm điểm, xếp loại chất lượng
+                        </a>
+                    </div>
+                </div>
+
+                <div id="mauphieu" class="row font-20 d-none">
                     <div class="col-md-6 mb-3">
                         <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
                             Phiếu xếp loại chất lượng đảng viên
@@ -34,50 +72,30 @@
                         </a>
                     </div>
                     <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
+                        <a href="#" class="btn btn-info">
                             Phiếu đánh giá đảng bộ khoa CNTT&TT
                         </a>
                     </div>
                     <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
+                        <a href="#" class="btn btn-info">
                             Phiếu xin ý kiến - Đánh giá chất lượng và xếp loại (tổ chức Đảng hoặc tập thể lãnh đạo)
                         </a>
                     </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Tổng hợp đề xuất mức chất lượng đảng viên của các chủ thể
-                        </a>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Quyết định - Công nhận xếp loại chất lượng đảng viên
-                        </a>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Quyết định - Về việc khen thưởng đảng viên
-                        </a>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Quyết định - Xếp loại chất lượng các chi bộ trực thuộc
-
-                        </a>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Quyết định - Tặng giấy khen cho chi bộ trực thuộc Đảng ủy cơ sở
-                        </a>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <a href="{{route('get.in.printMau1')}}" class="btn btn-info">
-                            Danh sách đảng viên chưa kiểm điểm, xếp loại chất lượng
-                        </a>
-                    </div>
-
                 </div>
             </div>
         </div>
     </div>
 
 @endsection
+@push('js')
+    <script>
+        $('#btn-maudon').click(function () {
+            $('#maudon').removeClass('d-none');
+            $('#mauphieu').addClass('d-none');
+        });
+        $('#btn-mauphieu').click(function () {
+            $('#mauphieu').removeClass('d-none');
+            $('#maudon').addClass('d-none');
+        });
+    </script>
+@endpush
